e17f177dd5996e8fb47ebd98baf2b2c344326ed5
Julien Capellari <julien.capellari@gmail.com>
2022-10-20 23:29:25 +0200

    feat: manager added event

--- src/task-manager.ts
+++ src/task-manager.ts
@@ -11,6 +11,7 @@ export interface TaskManagerOpts {
 }
 
 export type TaskManagerEventMap<C extends TaskContext = TaskContext> = {
+  added: Task<C>;
   started: Task<C>;
   completed: Task<C>;
 }
@@ -46,10 +47,14 @@ export class TaskManager<C extends TaskContext = TaskContext> extends EventSourc
       return;
     }
 
-    // Add task and it's dependencies
+    // Add task
     this._tasks.push(task);
     this._index.add(task);
 
+    // Prepare event emit
+    queueMicrotask(() => this.emit('added', task));
+
+    // Add task's dependencies
     for (const t of task.dependencies) {
       this._add(t);
     }
