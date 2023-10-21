1b8c26d2c83268fa829a504a2e4ca45249f9c2a4
Julien Capellari <julien.capellari@gmail.com>
2023-10-22 00:27:36 +0200

    feat: add fallback group

---
+++ src/groups/fallback-group.ts
@@ -0,0 +1,54 @@
+import { waitFor$ } from '@jujulego/event-tree';
+
+import { GroupTask } from './group-task.js';
+import { Task, TaskContext } from '../task.js';
+
+// Class
+export class FallbackGroup<C extends TaskContext = TaskContext> extends GroupTask<C> {
+  // Attributes
+  private _stopped = false;
+  private _currentTask?: Task;
+
+  // Methods
+  protected async* _orchestrate(): AsyncGenerator<Task> {
+    for (const task of this.tasks) {
+      if (this._stopped) {
+        this.setStatus('failed');
+        return;
+      }
+
+      // Start task
+      this._currentTask = task;
+      yield task;
+
+      // Wait task end
+      const result = await waitFor$(task, 'completed');
+
+      if (result.status === 'done') {
+        this.setStatus('done');
+        return;
+      }
+    }
+
+    this.setStatus('failed');
+  }
+
+  protected _stop(): void {
+    this._stopped = true;
+
+    // Stop current task
+    this._currentTask?.stop();
+  }
+
+  complexity(cache: Map<string, number> = new Map()): number {
+    let complexity = super.complexity(cache);
+
+    if (this.tasks.length > 0) {
+      complexity += this.tasks[0]!.complexity(cache);
+    }
+
+    cache.set(this.id, complexity);
+
+    return complexity;
+  }
+}
