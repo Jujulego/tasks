415b78871bfc25884791de4f47eb7735ea0deca3
Julien Capellari <julien.capellari@gmail.com>
2025-08-30 15:48:02 +0200

    test: spawn$ callbacks

--- tests/spawn$.test.ts
+++ tests/spawn$.test.ts
@@ -1,5 +1,5 @@
-import { spawn$, type SpawnTask$, task$, type TaskOnStartProps } from '@/src/index.js';
-import { execFile, type ChildProcess } from 'node:child_process';
+import { spawn$, type SpawnTask$, task$, type TaskOnStartProps, TaskState } from '@/src/index.js';
+import { type ChildProcess, execFile } from 'node:child_process';
 import { type Readable } from 'node:stream';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 
@@ -29,27 +29,29 @@ describe('spawn$', () => {
 
   describe('callbacks', () => {
     let task: SpawnTask$;
-    let onStart: (this: void, props: TaskOnStartProps) => Promise<void> | void;
-    let onCancel: (this: void) => Promise<void> | void;
+    let child: ChildProcess;
+    let onStart: (this: void, props: TaskOnStartProps) => void;
+    let onCancel: (this: void) => Promise<void>;
 
     beforeEach(() => {
       task = spawn$('echo', ['Hello World!'], { cwd: '/test' });
 
       onStart = vi.mocked(task$).mock.calls[0]![0].onStart;
-      onCancel = vi.mocked(task$).mock.calls[0]![0].onCancel!;
+      onCancel = vi.mocked(task$).mock.calls[0]![0].onCancel! as (this: void) => Promise<void>;
 
-      vi.mocked(execFile).mockReturnValue({
+      child = {
         once: vi.fn(),
         stdout: { pipe: vi.fn() } as unknown as Readable,
         stderr: { pipe: vi.fn() } as unknown as Readable,
-      } as unknown as ChildProcess);
+      } as unknown as ChildProcess;
+
+      vi.mocked(execFile).mockReturnValue(child);
     });
 
-    it('should call execFile to spawn process', async () => {
+    it('should call execFile to spawn process', () => {
       const controller = new AbortController();
-      const setState = vi.fn();
 
-      await onStart({ signal: controller.signal, setState });
+      onStart({ signal: controller.signal, setState: vi.fn() });
 
       expect(execFile).toHaveBeenCalledWith('echo', ['Hello World!'], {
         shell: true,
@@ -59,6 +61,81 @@ describe('spawn$', () => {
         cwd: '/test',
         env: expect.anything()
       });
+    });
+
+    it('should set task state to running when process is spawned', () => {
+      const controller = new AbortController();
+      const setState = vi.fn();
+
+      onStart({ signal: controller.signal, setState });
+
+      expect(child.once).toHaveBeenCalledWith('spawn', expect.any(Function));
+      expect(setState).not.toHaveBeenCalled();
+
+      // Call "spawn" event callback
+      vi.mocked(child.once).mock.calls.find((call) => call[0] === 'spawn')![1]();
+
+      expect(setState).toHaveBeenCalledWith(TaskState.Running);
+    });
+
+    it('should set task state to failed when process is errored', () => {
+      const controller = new AbortController();
+      const setState = vi.fn();
+
+      onStart({ signal: controller.signal, setState });
+
+      expect(child.once).toHaveBeenCalledWith('error', expect.any(Function));
+      expect(setState).not.toHaveBeenCalled();
+
+      // Call "spawn" event callback
+      vi.mocked(child.once as ((event: string, cb: (...args: unknown[]) => void) => void))
+        .mock.calls.find((call) => call[0] === 'error')![1]();
+
+      expect(setState).toHaveBeenCalledWith(TaskState.Failed);
+    });
+
+    it('should set task state to succeeded when process closes with exit code 0', async () => {
+      const controller = new AbortController();
+      const setState = vi.fn();
+      const cancelResolved = vi.fn();
+
+      onStart({ signal: controller.signal, setState });
+      void onCancel().then(cancelResolved);
+
+      expect(child.once).toHaveBeenCalledWith('close', expect.any(Function));
+      expect(setState).not.toHaveBeenCalled();
+      expect(cancelResolved).not.toHaveBeenCalled();
+
+      // Call "spawn" event callback
+      vi.mocked(child.once as ((event: string, cb: (...args: unknown[]) => void) => void))
+        .mock.calls.find((call) => call[0] === 'close')![1](0, null);
+
+      expect(setState).toHaveBeenCalledWith(TaskState.Succeeded);
+      expect(task.exitCode).toBe(0);
+
+      await vi.waitFor(() => expect(cancelResolved).toHaveBeenCalled());
+    });
+
+    it('should set task state to failed when process closes with exit code 1', async () => {
+      const controller = new AbortController();
+      const setState = vi.fn();
+      const cancelResolved = vi.fn();
+
+      onStart({ signal: controller.signal, setState });
+      void onCancel().then(cancelResolved);
+
+      expect(child.once).toHaveBeenCalledWith('close', expect.any(Function));
+      expect(setState).not.toHaveBeenCalled();
+      expect(cancelResolved).not.toHaveBeenCalled();
+
+      // Call "spawn" event callback
+      vi.mocked(child.once as ((event: string, cb: (...args: unknown[]) => void) => void))
+        .mock.calls.find((call) => call[0] === 'close')![1](1, null);
+
+      expect(setState).toHaveBeenCalledWith(TaskState.Failed);
+      expect(task.exitCode).toBe(1);
+
+      await vi.waitFor(() => expect(cancelResolved).toHaveBeenCalled());
     });
   });
 });
