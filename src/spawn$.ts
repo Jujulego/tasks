68d53ec8ef7bffc64c05a2ce5ec1477b3c5ebfa9
Julien Capellari <julien.capellari@gmail.com>
2025-08-30 12:13:45 +0200

    feat: compute spawn task id using props

--- src/spawn$.ts
+++ src/spawn$.ts
@@ -1,6 +1,7 @@
 import { type Observable, type Ref, var$, waitFor$ } from 'kyrielle';
 import { execFile } from 'node:child_process';
-import stream from 'node:stream';
+import { createHash } from 'node:crypto';
+import { Duplex, type Readable } from 'node:stream';
 import { type Task$, task$, type TaskProps } from './task$.js';
 import { TaskState } from './task-state.js';
 
@@ -8,14 +9,16 @@ import { TaskState } from './task-state.js';
  * Creates a task spawning a process in a shell.
  * @since 3.0.0
  */
-export function spawn$(cmd: string, args: readonly string[], { cwd, env, ...rest }: SpawnTaskProps): SpawnTask$ {
+export function spawn$(cmd: string, args: readonly string[], props: SpawnTaskProps = {}): SpawnTask$ {
+  const { id, cwd = process.cwd(), env, ...rest } = props;
   const closed$ = var$();
   const exitCode$ = var$<number>();
-  const stdout = new stream.Duplex({ allowHalfOpen: false });
-  const stderr = new stream.Duplex({ allowHalfOpen: false });
+  const stdout = new Duplex({ allowHalfOpen: false });
+  const stderr = new Duplex({ allowHalfOpen: false });
 
   const task = task$({
     ...rest,
+    id: id || createSpawnTaskId(cmd, args, cwd),
     onStart({ signal, setState }) {
       // TODO: escape args & pass them as string, to resolve DEP0190
       const spawned = execFile(cmd, args, {
@@ -23,7 +26,7 @@ export function spawn$(cmd: string, args: readonly string[], { cwd, env, ...rest
         windowsHide: true,
         signal,
         killSignal: 'SIGTERM',
-        cwd: cwd ?? process.cwd(),
+        cwd,
         env: { ...process.env, ...env },
       });
 
@@ -67,16 +70,29 @@ export function spawn$(cmd: string, args: readonly string[], { cwd, env, ...rest
   };
 }
 
+function createSpawnTaskId(cmd: string, args: readonly string[], cwd: string) {
+  const hash = createHash('md5');
+
+  hash.update(cwd);
+  hash.update(cmd);
+
+  for (const arg of args) {
+    hash.update(arg);
+  }
+
+  return hash.digest('hex');
+}
+
 export interface SpawnTask$ extends Task$ {
   /**
    * Spawned process stdout stream.
    */
-  readonly stdout: stream.Readable;
+  readonly stdout: Readable;
 
   /**
    * Spawned process stderr stream.
    */
-  readonly stderr: stream.Readable;
+  readonly stderr: Readable;
 
   /**
    * Spawned process exit code.
