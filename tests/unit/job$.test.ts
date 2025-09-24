6336ab66553db2e02f0cedb3f0dff4b9ed09aa5e
Julien Capellari <julien.capellari@gmail.com>
2025-09-24 23:30:22 +0200

    feat: test job$

--- tests/unit/job$.test.ts
+++ tests/unit/job$.test.ts
@@ -1,5 +1,111 @@
-import { isJobActive, isJobCompleted, isJobWaiting, JobState } from '@/src/index.js';
-import { describe, expect, it } from 'vitest';
+import {
+  isJobActive,
+  isJobCompleted,
+  isJobWaiting,
+  job$,
+  type JobOnStartProps,
+  JobState,
+} from '@/src/index.js';
+import { describe, expect, it, vi } from 'vitest';
+
+describe('job$', () => {
+  it('should create a job in ready state, with defaults applied', () => {
+    const job = job$({ onStart: vi.fn() });
+
+    expect(job.id).toBeDefined();
+    expect(job.state()).toBe(JobState.Ready);
+    expect(job.weight).toBe(1);
+  });
+
+  it('should apply given id', () => {
+    const job = job$({ id: 'life', onStart: vi.fn() });
+
+    expect(job.id).toBe('life');
+  });
+
+  it('should apply given weight', () => {
+    const job = job$({ weight: 42, onStart: vi.fn() });
+
+    expect(job.weight).toBe(42);
+  });
+
+  describe('start', () => {
+    it('should update job state to starting and call onStart callback', async () => {
+      const onStart = vi.fn();
+      const job = job$({ onStart });
+
+      await job.start();
+
+      expect(job.state()).toBe(JobState.Starting);
+      expect(onStart).toHaveBeenCalled();
+    });
+
+    it.each([
+      JobState.Running,
+      JobState.Succeeded,
+      JobState.Failed,
+    ] as const)('should apply running state as triggerred by onStart callback', async (state) => {
+      const onStart = vi.fn(({ setState }: JobOnStartProps) => setState(state));
+      const job = job$({ onStart });
+
+      await job.start();
+
+      expect(job.state()).toBe(state);
+    });
+
+    it('should update job state to failed if onStart callback throws', async () => {
+      const onStart = vi.fn(() => {
+        throw new Error('Test');
+      });
+      const job = job$({ onStart });
+
+      await expect(job.start()).rejects.toThrow(new Error('Test'));
+
+      expect(job.state()).toBe(JobState.Failed);
+    });
+
+    it('should throw if job is not waiting', async () => {
+      const job = job$({ onStart: vi.fn() });
+
+      await job.start();
+      await expect(job.start()).rejects.toThrow(new Error('Job in "starting" state cannot be started.'));
+    });
+  });
+
+  describe('cancel', () => {
+    it('should update job state to cancelled and trigger onStart signal', async () => {
+      const onStart = vi.fn<(props: JobOnStartProps) => void>();
+      const job = job$({ onStart });
+
+      // First start the job
+      await job.start();
+
+      const { signal } = onStart.mock.calls[0]![0];
+      expect(signal.aborted).toBe(false);
+
+      // Then cancel it !
+      await job.cancel();
+
+      expect(job.state()).toBe(JobState.Canceled);
+      expect(signal.aborted).toBe(true);
+    });
+
+    it('should call onCancel callback', async () => {
+      expect.assertions(3);
+
+      const onCancel = vi.fn(() => {
+        expect(job.state()).toBe(JobState.Canceling);
+      });
+      const job = job$({ onStart: vi.fn(), onCancel });
+
+      await job.start();
+      await job.cancel();
+
+      expect(job.state()).toBe(JobState.Canceled);
+      expect(onCancel).toHaveBeenCalled();
+    });
+  });
+});
 
 describe('isJobWaiting', () => {
   it.each([
