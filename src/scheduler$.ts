a3f8fcc0dc1dcd677a9e02777d4bde7e4c27a2a2
Julien Capellari <julien.capellari@gmail.com>
2025-10-04 14:29:57 +0200

    feat: add scheduler events

--- src/scheduler$.ts
+++ src/scheduler$.ts
@@ -1,6 +1,6 @@
-import { filter$, once$, pipe$, reduce$ } from 'kyrielle';
+import { filter$, type Multiplexer, multiplexer$, once$, pipe$, reduce$, type Source, source$ } from 'kyrielle';
 import { cpus } from 'node:os';
-import { isWorkloadEnded, WorkloadState } from './enums/workload-state.js';
+import { isWorkloadEnded, isWorkloadWaiting, WorkloadState } from './enums/workload-state.js';
 import type { Workload$ } from './workload$.js';
 
 // Constants
@@ -13,6 +13,13 @@ export const DEFAULT_STRENGTH = Math.max(cpus().length - 1, 1);
  */
 export function scheduler$(props: SchedulerProps): Scheduler$ {
   const { strength = DEFAULT_STRENGTH } = props;
+
+  const events$ = multiplexer$({
+    added: source$<Workload$>(),
+    started: source$<Workload$>(),
+    ended: source$<Workload$>(),
+  });
+
   const running = new Set<Workload$>();
   const queue: Workload$[] = [];
 
@@ -36,21 +43,31 @@ export function scheduler$(props: SchedulerProps): Scheduler$ {
 
       once$(isEnded$, () => {
         running.delete(workload);
+        events$.emit('ended', workload);
+
         void schedule();
       });
 
       // Start workload
       await workload.start();
+      events$.emit('started', workload);
+
       running.add(workload);
       runningWeight += workload.weight;
     }
   }
 
   return {
+    events$,
     strength,
 
     register(workload: Workload$) {
+      if (!isWorkloadWaiting(workload.state())) {
+        throw new Error(`Cannot schedule a workload in ${workload.state()} state`);
+      }
+
       queue.unshift(workload);
+      events$.emit('added', workload);
 
       // Schedule new workloads once this one is ready
       const isReady$ = pipe$(
@@ -76,7 +93,16 @@ export interface Scheduler$ {
   /**
    * Scheduler's total strength, limits the total weight of running workloads.
    */
-  strength: number;
+  readonly strength: number;
+
+  /**
+   * Scheduler's events
+   */
+  readonly events$: Multiplexer<{
+    'added': Source<Workload$>,
+    'started': Source<Workload$>,
+    'ended': Source<Workload$>,
+  }>;
 
   /**
    * Register workload
