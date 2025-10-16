d8d2d7f3e37200cfdd945dfcabd7d82f50b21646
Julien Capellari <julien.capellari@gmail.com>
2025-10-16 19:05:06 +0200

    feat: test sequence flow

---
+++ tests/unit/sequence-flow$.test.ts
@@ -0,0 +1,126 @@
+import { registry$ } from '@/src/bases/registry$.js';
+import { WorkloadState } from '@/src/enums/workload-state.js';
+import { sequenceFlow$ } from '@/src/sequence-flow$.js';
+import { workflow$, type Workflow$ } from '@/src/workflow$.js';
+import { workload$ } from '@/src/workload$.js';
+import { var$ } from 'kyrielle';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+
+// Mocks
+vi.mock('@/src/workflow$.js');
+
+beforeEach(() => {
+  vi.resetAllMocks();
+});
+
+// Tests
+describe('sequenceFlow$', () => {
+  it('should build a workflow', () => {
+    const workflow = {} as Workflow$;
+    vi.mocked(workflow$).mockReturnValue(workflow);
+
+    expect(sequenceFlow$()).toBe(workflow);
+
+    expect(workflow$).toHaveBeenCalledWith({
+      weight: 0,
+      onOrchestrate: expect.any(Function),
+      onCancel: expect.any(Function),
+    });
+  });
+
+  describe('onOrchestrate', () => {
+    it('should register workloads one after the other', async () => {
+      // Prepare elements
+      const reg = registry$();
+      vi.spyOn(reg, 'register');
+
+      const wkl1 = { id: '1', state$: var$(WorkloadState.Ready) };
+      const wkl2 = { id: '2', state$: var$(WorkloadState.Ready) };
+
+      const setState = vi.fn();
+      const controller = new AbortController();
+
+      // Call callback
+      sequenceFlow$();
+
+      const { onOrchestrate } = vi.mocked(workflow$).mock.calls[0]![0];
+      const prom = onOrchestrate([wkl1, wkl2] as unknown as Workflow$[], {
+        setState,
+        scheduler: reg,
+        signal: controller.signal,
+      });
+
+      expect(setState).toHaveBeenCalledWith(WorkloadState.Running);
+
+      // Register first workload
+      expect(reg.register).toHaveBeenCalledOnce();
+      expect(reg.register).toHaveBeenCalledWith(wkl1);
+      expect(reg.register).not.toHaveBeenCalledWith(wkl2);
+
+      // When first succeeds ...
+      wkl1.state$.mutate(WorkloadState.Succeeded);
+
+      await vi.waitFor(() => expect(reg.register).toHaveBeenCalledWith(wkl2));
+
+      // When second succeeds ...
+      wkl2.state$.mutate(WorkloadState.Succeeded);
+
+      await prom;
+
+      expect(setState).toHaveBeenCalledWith(WorkloadState.Succeeded);
+    });
+
+    it('should fail as one workload fails', async () => {
+      // Prepare elements
+      const reg = registry$();
+      vi.spyOn(reg, 'register');
+
+      const wkl1 = { id: '1', state$: var$(WorkloadState.Ready) };
+      const wkl2 = { id: '2', state$: var$(WorkloadState.Ready) };
+
+      const setState = vi.fn();
+      const controller = new AbortController();
+
+      // Call callback
+      sequenceFlow$();
+
+      const { onOrchestrate } = vi.mocked(workflow$).mock.calls[0]![0];
+      const prom = onOrchestrate([wkl1, wkl2] as unknown as Workflow$[], {
+        setState,
+        scheduler: reg,
+        signal: controller.signal,
+      });
+
+      expect(setState).toHaveBeenCalledWith(WorkloadState.Running);
+      expect(reg.register).toHaveBeenCalledWith(wkl1);
+
+      // When first fails ...
+      wkl1.state$.mutate(WorkloadState.Failed);
+
+      await prom;
+
+      expect(setState).toHaveBeenCalledWith(WorkloadState.Failed);
+      expect(reg.register).not.toHaveBeenCalledWith(wkl2);
+    });
+  });
+
+  describe('onCancel', () => {
+    it('should cancel all added workloads', async () => {
+      // Prepare workloads
+      const wkl1 = workload$({ onStart: vi.fn() });
+      vi.spyOn(wkl1, 'cancel').mockResolvedValue();
+
+      const wkl2 = workload$({ onStart: vi.fn() });
+      vi.spyOn(wkl2, 'cancel').mockResolvedValue();
+
+      // Call callback
+      sequenceFlow$();
+
+      const { onCancel } = vi.mocked(workflow$).mock.calls[0]![0];
+      await onCancel!([wkl1, wkl2]);
+
+      expect(wkl1.cancel).toHaveBeenCalledOnce();
+      expect(wkl2.cancel).toHaveBeenCalledOnce();
+    });
+  });
+});
