1ba7826d07fa553a93ab778d9f88060593f7da24
Julien Capellari <julien.capellari@gmail.com>
2025-10-19 21:56:02 +0200

    feat: add fallback flow

---
+++ src/fallback-flow$.ts
@@ -0,0 +1,38 @@
+import { filter$, pipe$, waitFor$ } from 'kyrielle';
+import { isWorkloadEnded, WorkloadState } from './enums/workload-state.js';
+import { workflow$, type WorkflowProps } from './workflow$.js';
+
+/**
+ * Creates a workflow running all it's tasks in sequence, until one succeeds.
+ * Only fails if all added workloads failed.
+ *
+ * @since 3.0.0
+ */
+export function fallbackFlow$(props: FallbackFlowProps = {}) {
+  return workflow$({
+    ...props,
+    weight: 0,
+    async onOrchestrate(workloads, { scheduler, setState, signal }) {
+      setState(WorkloadState.Running);
+
+      for (const workload of workloads) {
+        scheduler.register(workload);
+
+        const outcome = await waitFor$(pipe$(workload.state$, filter$(isWorkloadEnded)));
+        signal.throwIfAborted();
+
+        if (outcome === WorkloadState.Succeeded) {
+          setState(WorkloadState.Succeeded);
+          return;
+        }
+      }
+
+      setState(WorkloadState.Failed);
+    },
+    async onCancel(workloads) {
+      await Promise.all(workloads.map((wkl) => wkl.cancel()));
+    }
+  });
+}
+
+export type FallbackFlowProps = Omit<WorkflowProps, 'weight' | 'onOrchestrate' | 'onCancel'>;
--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,11 @@ export * from './bases/registry$.js';
 export * from './enums/workload-state.js';
 
 export * from './dependency$.js';
+export * from './fallback-flow$.js';
 export * from './job$.js';
 export * from './parallel-flow$.js';
 export * from './scheduler$.js';
+export * from './sequence-flow$.js';
 export * from './spawn$.js';
 export * from './unscheduler$.js';
 export * from './workflow$.js';
