715d9f8de56a35d15b5f06d72575c69d8dd7c1f2
Julien Capellari <julien.capellari@gmail.com>
2025-10-28 00:55:16 +0100

    feat: add recursive utils

--- src/utils/dependency.ts
+++ src/utils/dependency.ts
@@ -1,29 +1,54 @@
+import { filter$, iterator$, pipe$, type SimpleIterator } from 'kyrielle';
 import type { Registry$ } from '../bases/registry$.js';
-import { type Dependency$, isDependency$ } from '../dependency$.js';
+import { type Dependency, type Dependency$, isDependency$ } from '../dependency$.js';
 import { isWorkload$, type Workload$ } from '../workload$.js';
+
+/**
+ * Builds iterator on all dependencies of given element.
+ */
+export function allDependencies$(node: Dependency$): SimpleIterator<Dependency> {
+  const queue: Dependency[] = [node];
+  const marks = new Set(queue);
+
+  return iterator$({
+    next() {
+      const item = queue.shift();
+
+      if (!item) {
+        return { done: true };
+      }
+
+      if (isDependency$(item)) {
+        for (const dependency of item.dependencies) {
+          if (marks.has(dependency)) {
+            continue;
+          }
+
+          marks.add(dependency);
+          queue.push(dependency);
+        }
+      }
+
+      return {
+        done: false,
+        value: item
+      };
+    }
+  });
+}
 
 /**
  * Recursively registers workload and all its dependencies into given registry.
  */
 export function recursiveRegister(registry: Registry$, workload: Workload$ & Dependency$) {
-  const queue = [workload];
-  const marks = new Set(queue);
+  registry.register(workload);
 
-  while (queue.length) {
-    const item = queue.shift()!;
-    registry.register(item);
+  const dependencies = pipe$(
+    allDependencies$(workload),
+    filter$((dep) => isDependency$(dep) && isWorkload$(dep))
+  );
 
-    for (const dependency of item.dependencies) {
-      if (!isWorkload$(dependency) || !isDependency$(dependency)) {
-        continue;
-      }
-
-      if (marks.has(dependency)) {
-        continue;
-      }
-
-      marks.add(dependency);
-      queue.push(dependency);
-    }
+  for (const dependency of dependencies) {
+    registry.register(dependency);
   }
 }
