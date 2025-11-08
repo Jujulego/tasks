aba9bc5dd71fb4894c88cbdd81d7e5e89e71f7cf
Julien Capellari <julien.capellari@gmail.com>
2025-11-08 10:27:46 +0100

    Remove legacy classes (#544)
    * chore: remove legacy code
    
    * chore: bump version number
    
    * chore: use npm view

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-export * from './groups/fallback-group.js';
-export * from './groups/group-task.js';
-export * from './groups/parallel-group.js';
-export * from './groups/sequence-group.js';
-
-export * from './workers/worker-handler.js';
-export * from './workers/worker-pool.js';
-export * from './workers/worker-task.js';
-
-export * from './plan.js';
-export * from './spawn-task.js';
-export * from './task.js';
-export * from './task-manager.js';
-export * from './task-set.js';
-
 export * from './bases/registry$.js';
 
 export * from './enums/workload-state.js';
